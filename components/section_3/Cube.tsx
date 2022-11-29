018f7bec75b991b1632c03023f938808cfddba67
Zer0cheros <christian.wiss@optimaedu.fi>
2022-11-29 12:02:26 +0200

    a

--- components/section_3/Cube.tsx
+++ components/section_3/Cube.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import * as React from "react"
 import {Canvas, useLoader, useThree} from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import {TextureLoader} from 'three/src/loaders/TextureLoader'
 import { Flex, Box } from '@react-three/flex'
+import * as THREE from 'three'
+
 
 type Props = {
 
 }
+
 const Cube = () => {
   function Project(props:Props){
-    const texture= useLoader(TextureLoader, '/optidev.PNG')
+    const texture = useLoader(TextureLoader, '/optidev.PNG')
+    texture.encoding = THREE.sRGBEncoding
         return(
             <mesh {...props} >
-                <boxGeometry args={[4,4,4]}/>
+                <boxGeometry args={[3,3,3]}/>
                 <meshStandardMaterial map={texture} />
             </mesh>
         )
