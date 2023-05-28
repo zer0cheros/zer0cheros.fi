0c8de0892d9fcf5c87a6135f98f4a3d8b04d8929
Zer0cheros <christian.wiss@optimaedu.fi>
2023-05-28 18:12:26 +0300

    added more score to lighthouse

--- components/section_3/Section_3.tsx
+++ components/section_3/Section_3.tsx
@@ -8,8 +8,7 @@ const Work = dynamic(() => import('./Work'), {
 
 const Section_3 = () => {
   
-  return (
-    
+  return ( 
     <div id='project' className='w-full h-screen bg-white text-center'>
       <h1 className=' text-green-600 flex-none text-[5vw] w-auto font-medium'>Projects</h1>
       <Suspense fallback={'...loading'}>
