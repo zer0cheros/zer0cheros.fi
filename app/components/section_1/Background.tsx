6ce1a2afc8ff4532f516c8da09e79e9aaf36957a
zer0cheros <christian.wiss@optimaedu.fi>
2023-10-12 14:55:56 +0300

    fixed

--- app/components/section_1/Background.tsx
+++ app/components/section_1/Background.tsx
@@ -15,7 +15,7 @@ const Background = () => {
         <div className='absolute z-10 max-md:top-[15%] top-[10%] w-full h-[80%] text-center'>
             <div className='flex justify-center mt-10'>
               <Parallax offset={100}>
-                <h1 style={{textShadow:'2px 1px 8px black'}} className='text-green-600 text-6xl max-md:text-3xl' >Hi, I'm</h1>
+                <h1 style={{textShadow:'2px 1px 8px black'}} className='text-green-600 text-6xl max-md:text-3xl' >Hi, I&apos;m</h1>
                 <h1 style={{textShadow:'2px 1px 16px black'}} className='xl:text-[9rem] md:text-[5rem] text-[2.5rem]  font-medium text-gray-100 py-3 border-slate-50'>Zer0<span style={{textShadow:'2px 2px 12px black'}} className=' text-green-600 flex-none xl:text-[9rem] md:text-[5rem] text-[2.5rem] w-auto font-medium py-6'>cheros</span></h1>
               </Parallax>
             </div>
