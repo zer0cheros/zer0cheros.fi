dd65f496ea7a589d488e4b928692d3800405a202
zer0cheros <christian.wiss@optimaedu.fi>
2023-11-23 12:18:21 +0200

    small changes

--- app/components/section_3/Section_3.tsx
+++ app/components/section_3/Section_3.tsx
@@ -1,5 +1,6 @@
 import Work from "./Work"
 import { FadeIn } from "../FadeIn"
+import { motion } from "framer-motion"
 
 
 const Section_3 = () => {
@@ -8,7 +9,8 @@ const Section_3 = () => {
     <FadeIn>
     <div id='project' className='w-full h-screen bg-white text-center'>
       <h1 className=' text-green-700 flex-none text-[5vw] w-auto font-medium'>Projects</h1>
-        <Work/>
+      <motion.h2 initial={{x: -100}} animate={{x:0}} transition={{duration: 1}} className='text-center text-lg'>Drag cards to change, and doubletap to view info</motion.h2>
+        <Work/> 
     </div>
     </FadeIn>
     
