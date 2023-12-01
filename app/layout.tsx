7d05d4d44ae3f0b44d15b696e13e24c8cc8118a4
zer0cheros <christian.wiss@optimaedu.fi>
2023-12-01 10:54:31 +0200

    added SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
+import { DefaultSeo } from 'next-seo';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,9 +18,17 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <meta name="google-site-verification" content="Dyh_biTQza4pp3-P5BtTrjft_H9pha5eVGaFx6GsGbc" />
+      <meta charSet="utf-8" />
+      <DefaultSeo 
+      title={metadata.title}
+      description={metadata.description}
+      themeColor='#FFFFFF'
+      />
+      <meta name="viewport" content="width=device-width" />
       <body className={inter.className}>
         {children}           
         </body>
+        <Analytics />
     </html>
   )
 }
